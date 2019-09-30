0d28aaf8346bae763bccb652bd53a870d3da08ea
kapil jhajhria <jhajhria44@gmail.com>
2019-09-30 13:55:43 +0530

    day 1, Ex 1 done

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,22 @@ void main() {
   // Link to icon: https://mclarencollege.in/images/icon.png
   // Link to center image: https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg
 
-  String msg = 'Hello World';
-  Text textWidget = Text(msg);
-  Center center = Center(child: textWidget);
-  MaterialApp myApp = MaterialApp(home: center);
-  runApp(myApp);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('I Love Flutter')),
+          leading: Image(
+            image: NetworkImage('https://mclarencollege.in/images/icon.png'),
+          ),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://mclarencollege.in/images/9fc76a72-5b06-432f-b92e-d41d8ad5629f.jpg'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
